44188a6a7f91bc832678cce56ff37adddf4e7f04
EnigmaStudios <160673817+MatrixVerseSoftware@users.noreply.github.com>
2024-06-20 19:56:53 -0600

    Update script.js

--- script.js
+++ script.js
@@ -4,11 +4,17 @@ document.addEventListener('DOMContentLoaded', function() {
     // Crear elemento de audio
     var audio = new Audio('loadsoundtrident.mp3'); // Ruta al archivo de sonido
 
+    // Función para reproducir el sonido
+    function playSound() {
+        // Reproducir el sonido solo si aún no ha sido reproducido
+        if (!audio.paused && audio.currentTime > 0) {
+            audio.currentTime = 0; // Reiniciar el sonido si ya está reproduciéndose
+        }
+        audio.play();
+    }
+
     // Simular tiempo de carga (ajustado a 4 segundos)
     setTimeout(function() {
-        // Reproducir sonido al finalizar la carga
-        audio.play();
-
         // Ocultar overlay de carga
         var overlay = document.querySelector('.overlay');
         overlay.classList.add('fade-out');
@@ -18,5 +24,17 @@ document.addEventListener('DOMContentLoaded', function() {
             // Ocultar completamente el overlay
             overlay.style.display = 'none';
         });
+
+        // Reproducir sonido al finalizar la carga, solo si no hay interacción previa
+        if (document.hasFocus()) {
+            playSound();
+        } else {
+            // Evento para reproducir el sonido cuando el usuario interactúa con la página
+            document.addEventListener('click', playSoundOnce);
+            function playSoundOnce() {
+                playSound();
+                document.removeEventListener('click', playSoundOnce); // Remover el event listener después de reproducir una vez
+            }
+        }
     }, 4000); // Tiempo de carga simulado en milisegundos (4000 = 4 segundos)
 });
